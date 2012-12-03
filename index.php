2ba20d1acb1168aaa9403b4aeed41aa3c8e71bd4
Alejandro Acosta-Rubio <aacostarubio@gmail.com>
2012-12-03 01:23:25 -0600

    analytics working

--- index.php
+++ index.php
@@ -154,5 +154,18 @@ require_once($langfile);
 			<p>&nbsp;</p>
 		</div>
 	</div>
+	<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36675378-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 </body>
 </html>
